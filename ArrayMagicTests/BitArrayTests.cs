630f766bd2f06c2dbe9eabc7746bc33b2430e73f
Niels Abildgaard <niels.abildgaard@gmail.com>
2014-05-02 17:32:51 +0200

    Added BitArray test

---
+++ ArrayMagicTests/BitArrayTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArrayMagic;
+
+namespace ArrayMagicTests
+{
+    [TestClass]
+    public class BitArrayTests
+    {
+        [TestMethod]
+        public void TestIntegerOddFromBits()
+        {
+            var bytes = BitConverter.GetBytes(1337);
+            bool[] bits = bytes.ToBitArray();
+
+            //bit #0 is 1 => number is odd.
+            Assert.IsTrue(bits[0]);
+        }
+    }
+}
